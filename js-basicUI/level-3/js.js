27a8637cf680ec6b23e12357cf89d71f6105bb74
oilpaintingkv <oilpaintingkv@gmail.com>
2023-11-16 09:58:26 +0900

    data binding & client rendering

--- js-basicUI/level-3/js.js
+++ js-basicUI/level-3/js.js
@@ -1,12 +1,8 @@
 // car
-let car = ['k5', 50000, 'white'];
-let car2 = { name: 'lay', price: 50000, color: 'white' }; // key : value
-let card = document.querySelector('.card');
+let car2 = { name: 'k5', price: [50000, 3000, 4000] }; // key : value
+let price = document.querySelector('.price');
 
-console.log(car);
-console.log(car2.name);
-
-card.innerText = `name : ${car2.name} / price : ${car2.price} / color : ${car2.color}`;
+price.innerText = `price : ${car2.price[0]}`; // data binding (Put data into html)
 
 // tab
 let tabList = document.querySelector('.list');
