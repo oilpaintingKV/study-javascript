6d50f967df01a9be716e3f8cb4fe7d6c3f299666
oilpaintingkv <oilpaintingkv@gmail.com>
2023-11-17 18:01:39 +0900

    fetch

--- js-basicUI/level-3/js.js
+++ js-basicUI/level-3/js.js
@@ -1,17 +1,3 @@
-const products = [
-  { id: 0, price: 70000, title: 'Blossom Dress' },
-  { id: 1, price: 50000, title: 'Springfield Shirt' },
-  { id: 2, price: 60000, title: 'Black Monastery' },
-];
-
-const cardTitle = document.querySelectorAll('.card-tit');
-const cardPrice = document.querySelectorAll('.card-price');
-
-for (let i = 0; i < products.length; i++) {
-  cardTitle[i].innerText = products[i].title;
-  cardPrice[i].innerText = products[i].price;
-}
-
 // select
 const selectProduct = document.querySelector('.select_prod');
 const selectSize = document.querySelector('.select_size');
