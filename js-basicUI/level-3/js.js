4fa2e9a5e92de4642cc8739e6901c49f16a41f68
oilpaintingkv <oilpaintingkv@gmail.com>
2023-11-17 10:02:00 +0900

    create html

--- js-basicUI/level-3/js.js
+++ js-basicUI/level-3/js.js
@@ -19,9 +19,40 @@ const selectSize = document.querySelector('.select_size');
 selectProduct.addEventListener('input', (e) => {
   const value = e.currentTarget.value;
 
+  // shirt
   if (value === 'nice shirt') {
     selectSize.classList.add('show');
+
+    selectSize.innerHTML = '';
+
+    let shirtOption = ` <option>S</option>
+    <option>M</option>
+    <option>L</option>
+    <option>XL</option>`;
+    selectSize.insertAdjacentHTML('beforeend', shirtOption);
+  } else if (value == 'nice pants') {
+    selectSize.classList.add('show');
+
+    selectSize.innerHTML = '';
+
+    // 1. create Element
+    let optionTag = document.createElement('option');
+    optionTag.innerHTML = '28';
+    selectSize.appendChild(optionTag);
+
+    // 2. insertAdjacentHTML
+    let size30 = '<option>30</option>';
+    selectSize.insertAdjacentHTML('beforeend', size30);
   } else {
     selectSize.classList.remove('show');
   }
 });
+
+// create html - createElement (more faster)
+let a = document.createElement('p');
+a.innerHTML = 'hello';
+document.querySelector('#test').appendChild(a);
+
+// create html - insertAdjacentHTML
+let template = '<p>hi~</p>';
+document.querySelector('#test').insertAdjacentHTML('beforeend', template);
